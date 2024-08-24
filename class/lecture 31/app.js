c39317d3cd95d960578cfd070b4ee827114a959c
heyitsharman <harmankaur0779@gmail.com>
2024-08-24 22:16:17 +0530

    lectures

---
+++ class/lecture 31/app.js
@@ -0,0 +1,28 @@
+// let a=10;
+// console.log(a);
+
+// let b="hello world";
+// console.log(b);
+
+// alert("notification");
+
+// var was used earlier to define variables
+
+// var
+
+// var a=10;
+// var a=12;  //it can be re-declared and updated 
+// a=20;
+// console.log(a);
+
+
+// // let  
+// let a=10; 
+// a=20;  //we can update the value but can't re-declare it
+// console.log(a);
+
+// const  
+//we cant re-declare and update using const
+// const a=7;
+// console.log(a);
+
